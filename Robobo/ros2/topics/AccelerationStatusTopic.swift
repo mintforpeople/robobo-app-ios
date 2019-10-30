f86cce827d46e6189486c5a9fdeb802ff1487258
Luis Felipe Llamas Luaces <lfllamas93@gmail.com>
2019-10-30 17:46:57 +0100

    Added copyright headers

--- Robobo/ros2/topics/AccelerationStatusTopic.swift
+++ Robobo/ros2/topics/AccelerationStatusTopic.swift
@@ -1,3 +1,20 @@
+/*******************************************************************************
+*
+*   Copyright 2019, Manufactura de Ingenios Tecnológicos S.L.
+*   <http://www.mintforpeople.com>
+*
+*   Redistribution, modification and use of this software are permitted under
+*   terms of the Apache 2.0 License.
+*
+*   This software is distributed in the hope that it will be useful,
+*   but WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND; without even the implied
+*   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   Apache 2.0 License for more details.
+*
+*   You should have received a copy of the Apache 2.0 License along with
+*   this software. If not, see <http://www.apache.org/licenses/>.
+*
+******************************************************************************/
 
 //
 //  AccelerationStatusTopic.swift
